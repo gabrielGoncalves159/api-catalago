7c8552947754a95b729e223c46413ccad51c4a2a
Gabriel Gonçalves <ga.goncalves159753@gmail.com>
2025-05-02 14:57:36 -0300

    inclusão do EntityFramework e configurações iniciais

---
+++ APICatalago/APICatalago/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using APICatalago.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalago.Context
+{
+    public class AppDbContext : DbContext
+    {
+        //Recebe como parâmetro as opções de configurações que serão usadas para configurar o context do banco de dados
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+        }
+
+        //Definição de mapeamento de entidades das classes
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
+    }
+}
