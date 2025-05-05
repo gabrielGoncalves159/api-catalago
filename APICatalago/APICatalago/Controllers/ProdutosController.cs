c89b45fb3ab77ce87b91f34f6b2dbc09409cbdd4
Gabriel Gonçalves <ga.goncalves159753@gmail.com>
2025-05-05 20:54:51 -0300

    Criação controller de produtos

---
+++ APICatalago/APICatalago/Controllers/ProdutosController.cs
@@ -0,0 +1,81 @@
+﻿using APICatalago.Context;
+using APICatalago.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalago.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> ConsultaProdutos()
+        {
+            var produtos = _context.Produtos.ToList();
+            if (!produtos.Any())
+            {
+                return NotFound("Produtos não encontrados...");
+            }
+            return produtos;
+        }
+
+        [HttpGet("{id:int}", Name = "ObterProduto")]
+        public ActionResult<Produto> ConsultaProduto(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(x => x.ProdutoId == id);
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado...");
+            }
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult CadastraProduto(Produto produto)
+        {
+            if (produto is null) return BadRequest();
+
+            _context.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult AlterarProduto(int id, Produto produto)
+        {            
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult ExcluirProduto(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(x => x.ProdutoId == id);
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado...");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+    }
+}
--- APICatalago/APICatalago/Program.cs
+++ APICatalago/APICatalago/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Configuração para mapeamento dos controllers "AddControllers" e "MapControllers"
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
